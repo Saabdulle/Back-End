2467c7dcc03baa7a4fab76e52ce141460c97ddc9
“saabdulle” <saeed4640@gmail.com>
2023-01-17 10:52:11 +0000

    Set up data store

--- Server/app.js
+++ Server/app.js
@@ -1,13 +1,25 @@
 const express = require("express"); // Access to the Express library
 
+const {goats, nextId} = require("./goats")
 const app = express(); //Make a very basic server using Express.
 
 //Tell the app what kind of request to listen for land how to hand them.
 
 //req = request || res = respond
 
+// app.get("/", (req, res) => {
+//     res.send("Hell0, World!");
+// })
 app.get("/", (req, res) => {
-    res.send("Hell0, World!");
+    res.json({
+        "message": "Hello, World!"
+    })
+});
+
+app.get("/goats", (req, res) => {
+    res.json(goats)
 })
 
-module.exports = app //Make the server available to other files
+
+module.exports = app;
+// Make the server available to other files
---
+++ Server/goats.js
@@ -0,0 +1,25 @@
+const goats = [
+    {
+        "id": 1,
+        "name": "Billy",
+        "age": 6,
+        "sex": "M",
+        "favouriteColour": "magenta"
+    },
+    {   
+        "id": 2,
+        "name": "Barbara",
+        "age": 66,
+        "sex": "F",
+        "favouriteColour": "white"
+    },
+    {   
+        "id": 3,
+        "name": "Bob",
+        "age": 666,
+        "sex": "M",
+        "favouriteColour": "black"
+    }
+]
+let nextId = 4;
+module.exports = {goats, nextId};
